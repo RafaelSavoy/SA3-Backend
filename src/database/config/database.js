f118406cc0536e01df91519f970a0847f64f9880
RafaelSavoy <rafael@provsolutions.com.br>
2023-12-14 14:23:19 -0300

    Changing database

--- src/database/config/database.js
+++ src/database/config/database.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 module.exports = {
-  dialect: 'mysql',
+  dialect: 'postgres',
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
